9df8e3d2388229e55cde227bad6970984ee698c8
Josue-cloudU <josue.avila17@tectijuana.edu.mx>
2021-01-17 12:50:11 -0800

    listas de usuarios con queryset eliminar y banear

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,9 @@ from accounts import views
 
 urlpatterns = [
     path('listar_usuarios/', views.ListadoUsuarios.as_view(), name='listar_usuarios'),
+    path('listar_usuarios_admin/', views.ListadoUsuariosS.as_view(), name='listar_usuarioss'),
+    path('listar_usuarios_ban/', views.ListadoUsuariosB.as_view(), name='listar_usuariosb'),
     path('user_registerstaff/', views.RegistrarUsuarioS.as_view(), name="registerstaff"),
+    path('user_ban/<int:pk>', views.banearUsuario.as_view(), name="userban"),
+    path('user_delete/<int:pk>', views.delete.as_view(), name="user_delete"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,12 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 from django.views import generic
 from .forms import CustomUserForm, CustomUserStaffForm
 # Create your views here.
 
+# registrar usuarios desde login
 def Register(request):
     if request.method == 'POST':
         form = CustomUserForm(request.POST)
@@ -15,13 +17,14 @@ def Register(request):
             # #autenticar al usuario y redirigirlo al inicio
             # username = form.cleaned_data.get('username')
             # raw_password = form.cleaned_data.get('password')
-            # user = authenticate(username=username, password=raw_password)
+            # user = authenticate(username=username, password=raw_passwyyord)
             # login(request, user)
             return redirect('index')
     else:
         form = CustomUserForm()
     return render(request, 'accounts/register.html', {'form': form})
 
+# registrar usuarios con posibilidad de ser staff
 class RegistrarUsuarioS(LoginRequiredMixin, generic.CreateView):
     model = User
     form_class = CustomUserStaffForm
@@ -29,6 +32,7 @@ class RegistrarUsuarioS(LoginRequiredMixin, generic.CreateView):
     success_url = reverse_lazy("accounts:listar_usuarios")
     login_url = "login"
 
+# listar usuarios no baneados y no staff
 class ListadoUsuarios(LoginRequiredMixin, generic.ListView):
     model = User
     context_object_name = 'users'
@@ -36,8 +40,51 @@ class ListadoUsuarios(LoginRequiredMixin, generic.ListView):
     login_url = "login"
 
     def get_queryset(self):
-        return User.objects.filter(is_active = True).order_by('id')
+        return User.objects.filter(is_active = True, is_staff = False).order_by('id')
 
 # def ListadoUsuarios(request):
 #     return render(request,'accounts/listar_usuarios.html',{'users': User.objects.all()})
 #     # return render(self.model.objects.filter(is_active = True))
+
+# listar usuarios staff
+class ListadoUsuariosS(LoginRequiredMixin, generic.ListView):
+    model = User
+    context_object_name = 'users'
+    template_name = 'accounts/listar_usuarios.html'
+    login_url = "login"
+
+    def get_queryset(self):
+        return User.objects.filter(is_active = True, is_staff = True).order_by('id')
+
+# listar usuarios baneados
+class ListadoUsuariosB(LoginRequiredMixin, generic.ListView):
+    model = User
+    context_object_name = 'users'
+    template_name = 'accounts/listar_usuarios.html'
+    login_url = "login"
+
+    def get_queryset(self):
+        return User.objects.filter(is_active = False).order_by('id')
+
+# banear usuarios
+class banearUsuario(LoginRequiredMixin, generic.DeleteView):
+    model = User
+    template_name = 'accounts/modal/userban.html'
+    login_url = "login"
+
+    def post(self,request,pk,*args,**kwargs):
+        object = User.objects.get(id = pk)
+        object.is_active = False
+        object.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+# eliminar usuarios
+class delete(LoginRequiredMixin, generic.DeleteView):
+    model = User
+    template_name = 'accounts/modal/userelim.html'
+    login_url = "login"
+
+    def post(self,request,pk,*args,**kwargs):
+        object = User.objects.get(id = pk)
+        object.delete()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
