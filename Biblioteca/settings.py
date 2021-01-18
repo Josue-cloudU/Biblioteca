f1c58d4a3ec3e14a419771f5720f2b21651fd09a
Josue-cloudU <josue.avila17@tectijuana.edu.mx>
2021-01-18 11:39:19 -0800

    A

--- Biblioteca/settings.py
+++ Biblioteca/settings.py
@@ -139,7 +139,7 @@ DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Simplified static file serving.
